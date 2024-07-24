f53f32ab7ced57d6d4444f66bbd2316a6dc1eed4
chenyinheng30 <chenyinheng30@outlook.com>
2024-07-24 17:56:10 +0800

    可以接受 clang 的参数

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-source=$1
+prama=($@)
+source=${prama[0]}
 source=${source:=he.cu}
 arr=(${source//./ /})
 
-clang $source -o ${arr[0]} -lcudart_static -lcublas 2>/tmp/run_cuda.log
+clang++ $source -o ${arr[0]} -lcudart_static ${prama[@]:1} 2>/tmp/run_cuda_error.log
 if [ $? -eq 0 ]; then
     ./${arr[0]}
     rm ${arr[0]}
 else
-    cat /tmp/run_cuda.log 1>&2
+    cat /tmp/run_cuda_error.log 1>&2
+    rm /tmp/run_cuda_error.log
 fi
