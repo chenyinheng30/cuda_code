3780330e69f3bf0b5dcd37bc415b664abdeda482
chenyinheng30 <chenyinheng30@outlook.com>
2024-07-24 17:27:12 +0800

    自动化运行

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
-nvcc he.cu -o he
-./he
-rm he
+#!/bin/bash
+
+source=$1
+source=${source:=he.cu}
+arr=(${source//./ /})
+
+clang $source -o ${arr[0]} -lcudart_static -lcublas 2>/tmp/run_cuda.log
+if [ $? -eq 0 ]; then
+    ./${arr[0]}
+    rm ${arr[0]}
+else
+    cat /tmp/run_cuda.log 1>&2
+fi
